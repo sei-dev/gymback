3a79fe02ad1b6e112b7cd96f07cada68ca68b658
Nikola <nikola.bojovic9@gmail.com>
2024-10-18 18:28:58 +0200

    123

--- views/manager/termsofservice.php
+++ views/manager/termsofservice.php
@@ -6,54 +6,54 @@
     </div>
     
     <div class="box-footer" style="">
+    <h2>USLOVI KORIŠĆENJA</h2>
+    
         <p>
-        USLOVI KORIŠĆENJA
-
-Dobrodošli na našu web stranicu. Molimo vas da pažljivo pročitate ove Uslove korišćenja pre nego što počnete koristiti naš sajt. Korišćenjem sajta, saglasni ste sa Uslovima korišćenja i našom Politikom privatnosti.
-
-1. Opšte informacije
-Ovaj sajt je u vlasništvu i pod upravom firme "Personalni trener". Pristupom i korišćenjem ove stranice pristajete na uslove i pravila koja su ovde navedena. Ako se ne slažete sa bilo kojim delom ovih uslova, molimo vas da ne koristite naš sajt.
-
-2. Korišćenje usluga
-Sve informacije na ovoj stranici pružaju se u informativne svrhe i ne predstavljaju zamenu za profesionalni medicinski savet. Kao korisnik, prihvatate da odgovornost za vašu fizičku spremnost, zdravlje i bezbednost tokom vežbanja leži na vama. Konsultujte se sa vašim lekarom pre početka bilo kog programa treninga.
-
-3. Prava intelektualne svojine
-Svi sadržaji dostupni na ovom sajtu, uključujući tekstove, slike, video materijale i logo, su vlasništvo firme "Personalni trener" ili su licencirani za korišćenje. Neovlašćeno kopiranje, distribucija ili modifikacija bilo kojeg dela sajta bez našeg izričitog odobrenja je zabranjena.
-
-4. Izmene uslova korišćenja
-Zadržavamo pravo da promenimo ove Uslove korišćenja u bilo kom trenutku. Vaša dalja upotreba sajta nakon promena znači da prihvatate sve izmenjene uslove.
-
-5. Ograničenje odgovornosti
-"Personalni trener" neće biti odgovoran za bilo kakve direktne ili indirektne štete nastale korišćenjem informacija ili usluga sa sajta. Svi programi vežbanja sprovode se na vlastitu odgovornost korisnika.
-
-POLITIKA PRIVATNOSTI
-
-Ova Politika privatnosti objašnjava kako prikupljamo, koristimo i štitimo vaše podatke dok koristite našu web stranicu i usluge.
-
-1. Koje podatke prikupljamo?
-Prikupljamo lične podatke koje nam vi dobrovoljno dostavite, uključujući vaše ime, email adresu, broj telefona, informacije o fizičkom stanju i preferencije u vezi sa treningom. Ovi podaci nam omogućavaju da personalizujemo naše usluge prema vašim potrebama.
-
-2. Kako koristimo vaše podatke?
-Prikupljeni podaci koriste se za:
-
-Pružanje i prilagođavanje usluga treninga
-Komunikaciju sa vama o terminima treninga, novostima i promocijama
-Unapređenje korisničkog iskustva na sajtu
-Ne delimo vaše podatke sa trećim stranama osim ako je to zakonski zahtevano ili neophodno za pružanje usluga.
-
-3. Zaštita podataka
-Koristimo odgovarajuće tehničke i organizacione mere kako bismo zaštitili vaše podatke od neovlašćenog pristupa, gubitka ili izmene. Ipak, nijedna metoda prenosa preko interneta ili metoda elektronskog skladištenja nije 100% sigurna, te ne možemo garantovati apsolutnu bezbednost podataka.
-
-4. Kolačići (Cookies)
-Naša stranica koristi kolačiće kako bi unapredila funkcionalnost i korisničko iskustvo. Korišćenjem sajta, pristajete na upotrebu kolačića u skladu sa našom Politikom o kolačićima.
-
-5. Vaša prava
-Imate pravo da pristupite, ispravite ili izbrišete vaše lične podatke koje čuvamo. Takođe možete u bilo kom trenutku povući saglasnost za obradu vaših podataka. Ako želite da ostvarite neka od ovih prava, kontaktirajte nas na [email adresa].
-
-6. Izmene politike privatnosti
-Zadržavamo pravo da promenimo ovu Politiku privatnosti u bilo kom trenutku. Sve izmene će biti objavljene na ovoj stranici.
-
-Ako imate bilo kakva pitanja u vezi sa našim Uslovima korišćenja ili Politikom privatnosti, slobodno nas kontaktirajte na [email adresa].
+            Dobrodošli na našu web stranicu. Molimo vas da pažljivo pročitate ove Uslove korišćenja pre nego što počnete koristiti naš sajt. Korišćenjem sajta, saglasni ste sa Uslovima korišćenja i našom Politikom privatnosti.<br>
+            
+            1. Opšte informacije<br>
+            Ovaj sajt je u vlasništvu i pod upravom firme "Personalni trener". Pristupom i korišćenjem ove stranice pristajete na uslove i pravila koja su ovde navedena. Ako se ne slažete sa bilo kojim delom ovih uslova, molimo vas da ne koristite naš sajt.<br>
+            
+            2. Korišćenje usluga<br>
+            Sve informacije na ovoj stranici pružaju se u informativne svrhe i ne predstavljaju zamenu za profesionalni medicinski savet. Kao korisnik, prihvatate da odgovornost za vašu fizičku spremnost, zdravlje i bezbednost tokom vežbanja leži na vama. Konsultujte se sa vašim lekarom pre početka bilo kog programa treninga.<br>
+            
+            3. Prava intelektualne svojine<br>
+            Svi sadržaji dostupni na ovom sajtu, uključujući tekstove, slike, video materijale i logo, su vlasništvo firme "Personalni trener" ili su licencirani za korišćenje. Neovlašćeno kopiranje, distribucija ili modifikacija bilo kojeg dela sajta bez našeg izričitog odobrenja je zabranjena.<br>
+            
+            4. Izmene uslova korišćenja<br>
+            Zadržavamo pravo da promenimo ove Uslove korišćenja u bilo kom trenutku. Vaša dalja upotreba sajta nakon promena znači da prihvatate sve izmenjene uslove.<br>
+            
+            5. Ograničenje odgovornosti<br>
+            "Personalni trener" neće biti odgovoran za bilo kakve direktne ili indirektne štete nastale korišćenjem informacija ili usluga sa sajta. Svi programi vežbanja sprovode se na vlastitu odgovornost korisnika.</p>
+            
+            <h2>POLITIKA PRIVATNOSTI</h2>
+            
+            <p>Ova Politika privatnosti objašnjava kako prikupljamo, koristimo i štitimo vaše podatke dok koristite našu web stranicu i usluge.<br>
+            
+            1. Koje podatke prikupljamo?<br>
+            Prikupljamo lične podatke koje nam vi dobrovoljno dostavite, uključujući vaše ime, email adresu, broj telefona, informacije o fizičkom stanju i preferencije u vezi sa treningom. Ovi podaci nam omogućavaju da personalizujemo naše usluge prema vašim potrebama.<br>
+            
+            2. Kako koristimo vaše podatke?<br>
+            Prikupljeni podaci koriste se za:<br>
+            
+            Pružanje i prilagođavanje usluga treninga<br>
+            Komunikaciju sa vama o terminima treninga, novostima i promocijama<br>
+            Unapređenje korisničkog iskustva na sajtu<br>
+            Ne delimo vaše podatke sa trećim stranama osim ako je to zakonski zahtevano ili neophodno za pružanje usluga.<br>
+            
+            3. Zaštita podataka<br>
+            Koristimo odgovarajuće tehničke i organizacione mere kako bismo zaštitili vaše podatke od neovlašćenog pristupa, gubitka ili izmene. Ipak, nijedna metoda prenosa preko interneta ili metoda elektronskog skladištenja nije 100% sigurna, te ne možemo garantovati apsolutnu bezbednost podataka.<br>
+            
+            4. Kolačići (Cookies)<br>
+            Naša stranica koristi kolačiće kako bi unapredila funkcionalnost i korisničko iskustvo. Korišćenjem sajta, pristajete na upotrebu kolačića u skladu sa našom Politikom o kolačićima.<br>
+            
+            5. Vaša prava<br>
+            Imate pravo da pristupite, ispravite ili izbrišete vaše lične podatke koje čuvamo. Takođe možete u bilo kom trenutku povući saglasnost za obradu vaših podataka. Ako želite da ostvarite neka od ovih prava, kontaktirajte nas na [email adresa].<br>
+            
+            6. Izmene politike privatnosti<br>
+            Zadržavamo pravo da promenimo ovu Politiku privatnosti u bilo kom trenutku. Sve izmene će biti objavljene na ovoj stranici.<br>
+            
+            Ako imate bilo kakva pitanja u vezi sa našim Uslovima korišćenja ili Politikom privatnosti, slobodno nas kontaktirajte na [email adresa]</p>
         </p>
     </div>
     
