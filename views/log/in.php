d38cb65c3a54abaa4d88882969b082d05719a11e
Nikola <nikola.bojovic9@gmail.com>
2024-10-18 19:51:17 +0200

    123

--- views/log/in.php
+++ views/log/in.php
@@ -21,7 +21,7 @@
         </div>
         <div class="row">
             <div class="col-xs-8">
-               <a href = "/tof"><?=$this->translate("Terms of Service");?></a> 
+               <a href = "https://phpstack-1301327-4732761.cloudwaysapps.com/log/tof"><?=$this->translate("Terms of Service");?></a> 
             </div>
             
             <div class="col-xs-4">
