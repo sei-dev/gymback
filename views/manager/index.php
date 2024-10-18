2a19ee0f4aa2e4ceeb96d9cbb52b761a4eb92a76
Nikola <nikola.bojovic9@gmail.com>
2024-10-18 19:33:26 +0200

    123

--- views/manager/index.php
+++ views/manager/index.php
@@ -1,27 +1,26 @@
 <h3 class="heading-style"><?=$this->translate("Users");?></h3>
 
 <div class="box box-primary">
-    <div class="box-body">
-        <ul class="todo-list ui-sortable">
+    <div class="box-body bgr-every-second">
+        
         <?php foreach ($this->users as $one):?>
-        <li>
-            <!-- <span class="">
-            <i class="fa fa-ellipsis-v"></i>
-            </span> -->
-            <span class="text"><?=$one["first_name"]?> <?=$one["last_name"]?></span>
-            <span class="label label-success"><?=$one["email"]?></span>
-            <span class="label label-success"><?=$one["phone"]?></span>
-            <span class="label label-success"><?=$one["location"]?></span>
-            <span class="label label-success"><?=$one["deadline"] . "h"?></span>
-            <span class="label label-success"><?php if($one["is_trainer"]==0||$one["is_trainer"]=="0") echo $this->translate("Client"); else echo $this->translate("Trainer");?></span>
-            <small class="label label-danger"><i class="fa fa-clock-o"></i> <?=$this->humanReadable($one["created_on"])?></small>
-            <div class="tools">
+        <div class="row">
+            <div class="col-md-1"><span class="circular--portrait"> <img src="/images/blank.webp" /></span></div>
+            <div class="col-md-2"><?=$one["first_name"]?> <?=$one["last_name"]?></div>
+            <div class="col-md-2"><?=$one["email"]?></div>
+            <div class="col-md-2 text "><?=$one["phone"]?></div>
+            <div class="col-md-1 text "><?=$one["location"]?></div>
+            <div class="col-md-1 text "><?=$one["deadline"] . "h"?></div>
+            <div class="col-md-1 label label-success"><?php if($one["is_trainer"]=="0") echo $this->translate("Client"); else echo $this->translate("Trainer");?></div>
+            <div class="col-md-1 label label-danger"><i class="fa fa-clock-o"></i> <?=$this->humanReadable($one["created_on"])?></div>
+            <div class="col-md-1 tools">
                 <a href="/manager/edituser?id=<?=$one["id"]?>"><i class="fa fa-edit"></i></a>
                 <i class="fa fa-trash-o"></i>
             </div>
-        </li>
+        </div>
         <?php endforeach;?>
-        </ul>
+        
+        
     </div>
     
     <div class="box-footer" style="">
