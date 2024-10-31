40b1b9b818d718edd3652d7b05beb4342685e3ef
Nikola <nikola.bojovic9@gmail.com>
2024-10-31 13:07:51 +0100

    123

--- views/manager/gyms.php
+++ views/manager/gyms.php
@@ -2,6 +2,7 @@
 
 <div class="box">
 <form class="req-forms" id="search_form" method="post" action="searchgym" enctype="multipart/form-data">
+				<div class="row">
 				<div class="col-md-11">
 					<div class="form-group">
                 		<input placeholder="<?=$this->translate("Search");?>" class="form-control" type="text" name="param" value=""/>
@@ -10,6 +11,7 @@
                 	<div class="col-md-1">
                 		<button name="submit" type="submit" class="btn btn-primary btn-block btn-flat"><i class="fa fa-search"></i></button>
                 	</div>
+                </div>
 </form>
 </div>
 
