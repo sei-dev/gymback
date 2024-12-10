402e5d47cd8ee943afe17eb6eed910bc69d55b83
Nikola <nikola.bojovic9@gmail.com>
2024-12-10 18:53:46 +0100

    123

--- views/manager/measurements.php
+++ views/manager/measurements.php
@@ -2,35 +2,35 @@
 
 <div class="box box-primary">
     <div class="box-body bgr-every-second">
-    <!-- Header Row -->
+
     <div class="row font-weight-bold">
         <div class="col-md-2"><?=$this->translate("Trainer Name")?></div>
         <div class="col-md-2"><?=$this->translate("Client Name")?></div>
-        <div class="col-md-2"><?=$this->translate("Height (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Weight (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Waist (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Biceps (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Neck (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Chest (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Gluteus (cm)")?></div>
-        <div class="col-md-2"><?=$this->translate("Lower Leg (cm)")?></div>
+        <div class="col-md-2"><?=$this->translate("Height")?></div>
+        <div class="col-md-2"><?=$this->translate("Weight")?></div>
+        <div class="col-md-2"><?=$this->translate("Waist")?></div>
+        <div class="col-md-2"><?=$this->translate("Biceps")?></div>
+        <div class="col-md-2"><?=$this->translate("Neck")?></div>
+        <div class="col-md-2"><?=$this->translate("Chest")?></div>
+        <div class="col-md-2"><?=$this->translate("Gluteus")?></div>
+        <div class="col-md-2"><?=$this->translate("Lower Leg")?></div>
         <div class="col-md-1"><?=$this->translate("Measured At")?></div>
-        <div class="col-md-1"><?=$this->translate("Actions")?></div>
+        <div class="col-md-1"></div>
     </div>
 
-    <!-- Data Rows -->
+
     <?php foreach ($this->items as $one): ?>
         <div class="row">
             <div class="col-md-2"><?=$one["trainer_name"]?></div>
             <div class="col-md-2"><?=$one["client_name"]?></div>
-            <div class="col-md-2"><?=$one["height"]?></div>
-            <div class="col-md-2"><?=$one["weight"]?></div>
-            <div class="col-md-2"><?=$one["waist"]?></div>
-            <div class="col-md-2"><?=$one["biceps"]?></div>
-            <div class="col-md-2"><?=$one["neck"]?></div>
-            <div class="col-md-2"><?=$one["chest"]?></div>
-            <div class="col-md-2"><?=$one["gluteus"]?></div>
-            <div class="col-md-2"><?=$one["lower_leg"]?></div>
+            <div class="col-md-2"><?=$one["height"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["weight"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["waist"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["biceps"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["neck"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["chest"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["gluteus"] . " cm"?></div>
+            <div class="col-md-2"><?=$one["lower_leg"] . " cm"?></div>
             <div class="col-md-1 label label-danger">
                 <i class="fa fa-clock-o"></i> <?=$this->humanReadable($one["measured_at"])?>
             </div>
@@ -41,6 +41,7 @@
         </div>
     <?php endforeach; ?>
 </div>
+
     
     <div class="box-footer" style="">
         <i class="ion ion-clipboard">Total: <?=$this->count?></i>
