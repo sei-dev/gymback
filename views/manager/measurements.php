544727566189865883503ba410cceef19474bde7
Nikola <nikola.bojovic9@gmail.com>
2024-12-10 19:09:40 +0100

    123

--- views/manager/measurements.php
+++ views/manager/measurements.php
@@ -15,7 +15,7 @@
         <div class="col-md-2"><?=$this->translate("Gluteus")?></div>
         <div class="col-md-2"><?=$this->translate("Lower Leg")?></div>
         <div class="col-md-1"><?=$this->translate("Measured At")?></div>
-        <div class="col-md-1"><?=$this->translate("Actions")?></div>
+        <div class="col-md-1"><?=$this->translate("")?></div>
     </div>
 
 
