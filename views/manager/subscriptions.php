9141afd0e879be3a7b270839bbdcbe3a67f5e302
Nikola <nikola.bojovic9@gmail.com>
2024-12-11 11:18:57 +0100

    123

--- views/manager/subscriptions.php
+++ views/manager/subscriptions.php
@@ -4,17 +4,17 @@
     <div class="box-body bgr-every-second">
     
         <div class="row">
-        	<div class="col-md-6"><?=$this->translate("Monthly subscription")?></div>
+        	<div class="col-md-8"><?=$this->translate("Monthly subscription")?></div>
             <div class="col-md-3"><?=$this->items[0]["price"]?></div>
-            <div class="col-md-3 tools">
+            <div class="col-md-1 tools">
                 <a href="/manager/editsub?id=<?=$this->items[0]["id"]?>"><i class="fa fa-edit"></i></a>
             </div>
         </div>
         	
         <div class="row">
-        	<div class="col-md-6"><?=$this->translate("Yearly subscription")?></div>
+        	<div class="col-md-8"><?=$this->translate("Yearly subscription")?></div>
             <div class="col-md-3"><?=$this->items[1]["price"]?></div>
-            <div class="col-md-3 tools">
+            <div class="col-md-1 tools">
                 <a href="/manager/editsub?id=<?=$this->items[1]["id"]?>"><i class="fa fa-edit"></i></a>
             </div>
         </div>
