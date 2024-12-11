965ba62bf7f892fbd722c191e18e5bb33d932f08
Nikola <nikola.bojovic9@gmail.com>
2024-12-11 12:10:03 +0100

    123

--- views/layout/partials/menu.php
+++ views/layout/partials/menu.php
@@ -4,7 +4,7 @@
     	<a class="menuLinkProper" href="/manager/index" title="Users"><i class="fa fa-arrow-right"></i><span>Users</span></a>
     	<a class="menuLinkProper" href="/manager/gyms" title="Gyms"><i class="fa fa-arrow-right"></i><span>Gyms</span></a>
     	<!--<a class="menuLinkProper" href="/manager/trainers" title="Trainers"><i class="fa fa-arrow-right"></i><span>Trainers</span></a>-->
-    	<!--<a class="menuLinkProper" href="/manager/invoices" title="Invoices"><i class="fa fa-arrow-right"></i><span>Invoices</span></a>-->
+    	<a class="menuLinkProper" href="/manager/invoices" title="Invoices"><i class="fa fa-arrow-right"></i><span>Invoices</span></a>
  		<a class="menuLinkProper" href="/manager/trainings" title="Trainings"><i class="fa fa-arrow-right"></i><span>Trainings</span></a>
  		<a class="menuLinkProper" href="/manager/measurements" title="Measurements"><i class="fa fa-arrow-right"></i><span>Measurements</span></a>
  		<a class="menuLinkProper" href="/manager/cities" title="Cities"><i class="fa fa-arrow-right"></i><span>Cities</span></a>
