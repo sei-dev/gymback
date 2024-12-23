ca0dc216c6a4b68bab95cb66a072077ef42badde
Nikola <nikola.bojovic9@gmail.com>
2024-12-24 00:32:32 +0100

    123

--- views/manager/index.php
+++ views/manager/index.php
@@ -23,17 +23,17 @@
             <div class="col-md-1"><span class="circular--portrait"> <img src="<?= !empty($one["image"]) ? $one["image"] : '/images/ikonica.ico'; ?>" class="img-fluid" alt="User Image" /></span></div>
             <div class="col-md-2"><?=$one["first_name"]?> <?=$one["last_name"]?></div>
             <div class="col-md-2"><?=$one["email"]?></div>
-            <div class="col-md-2 text "><?=$one["phone"]?></div>
+            <div class="col-md-1 text "><?=$one["phone"]?></div>
             <div class="col-md-1 text "><?=$one["location"]?></div>
             <div class="col-md-1 text "><?=$one["deadline"] . "h"?></div>
             <div class="col-md-1 label label-success"><?php if($one["is_trainer"]=="0") echo $this->translate("Client"); else echo $this->translate("Trainer");?></div>
             <div class="col-md-1 label label-danger"><i class="fa fa-clock-o"></i> <?=$this->humanReadable($one["created_on"])?></div>
-            <div class="col-md-1 tools">
+            <div class="col-md-2 tools">
                 <a href="/manager/edituser?id=<?=$one["id"]?>"><i class="fa fa-edit"></i></a>
                	<a href="/manager/edituser?id=<?=$one["id"]?>"><i class="fa fa-industry"></i></a>
                	<a href="/manager/edituser?id=<?=$one["id"]?>"><i class="fa fa-file-archive-o"></i></a>
                 <a href="/manager/edituser?id=<?=$one["id"]?>"><i class="fa fa-address-book-o"></i></a>
-                <i class="fa fa-trash"></i>
+                <i class="fa fa-trash-o"></i>
             </div>
         </div>
         <?php endforeach;?>
