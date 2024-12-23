81cafdcaf36ba05d4b869af0aaa98941d3f6f7b1
Nikola <nikola.bojovic9@gmail.com>
2024-12-23 17:42:21 +0100

    123

--- config/prod.php
+++ config/prod.php
@@ -1,7 +1,12 @@
 <?php 
 return [
-    'host' => '64.225.9.163',
+    /* 'host' => '64.225.9.163',
     'username' => 'ckqyuwbdrh',
     'password' => 'SPHUA2R6v7',
-    'dbname' => 'ckqyuwbdrh'
+    'dbname' => 'ckqyuwbdrh' */
+    
+    'host' => '64.225.9.163',
+    'username' => 'xvvfqaxdrz',
+    'password' => '33KCMGtr95',
+    'dbname' => 'xvvfqaxdrz'
 ];
