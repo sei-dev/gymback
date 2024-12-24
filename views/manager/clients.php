87e2d906af16db09f23f5f17e11996ab28545749
Nikola <nikola.bojovic9@gmail.com>
2024-12-24 16:09:19 +0100

    123

--- views/manager/clients.php
+++ views/manager/clients.php
@@ -22,12 +22,11 @@
         <div class="col-md-1"><?=$this->translate("Profile Image")?></div>
         <div class="col-md-2"><?=$this->translate("Name")?></div>
         <div class="col-md-2"><?=$this->translate("Email")?></div>
-        <div class="col-md-1"><?=$this->translate("Phone")?></div>
+        <div class="col-md-2"><?=$this->translate("Phone")?></div>
         <div class="col-md-1"><?=$this->translate("Location")?></div>
-        <div class="col-md-1"><?=$this->translate("Deadline")?></div>
         <div class="col-md-1"><?=$this->translate("Role")?></div>
-        <div class="col-md-1"><?=$this->translate("Created At")?></div>
-        <div class="col-md-2"><?=$this->translate("Actions")?></div>
+        <div class="col-md-2"><?=$this->translate("Created At")?></div>
+        <div class="col-md-1"><?=$this->translate("Actions")?></div>
     </div>
         
         <?php foreach ($this->users as $one):?>
@@ -35,12 +34,11 @@
             <div class="col-md-1"><span class="circular--portrait"> <img src="<?= !empty($one["image"]) ? $one["image"] : '/images/ikonica.ico'; ?>" class="img-fluid" alt="User Image" /></span></div>
             <div class="col-md-2"><?=$one["first_name"]?> <?=$one["last_name"]?></div>
             <div class="col-md-2"><?=$one["email"]?></div>
-            <div class="col-md-1 text "><?=$one["phone"]?></div>
+            <div class="col-md-2 text "><?=$one["phone"]?></div>
             <div class="col-md-1 text "><?=$one["location"]?></div>
-            <div class="col-md-1 text "><?=$one["deadline"] . "h"?></div>
             <div class="col-md-1 label label-success"><?php if($one["is_trainer"]=="0") echo $this->translate("Client"); else echo $this->translate("Trainer");?></div>
-            <div class="col-md-1 label label-danger"><i class="fa fa-clock-o"></i> <?=$this->humanReadable($one["created_on"])?></div>
-            <div class="col-md-2 tools">
+            <div class="col-md-2 label label-danger"><i class="fa fa-clock-o"></i> <?=$this->humanReadable($one["created_on"])?></div>
+            <div class="col-md-1 tools">
                 <a href="/manager/edituser?id=<?=$one["id"]?>"><i class="fa fa-edit" title="Edit"></i></a>
                	<a href="/manager/usermeasurements?id=<?=$one["id"]?>"><i class="fa fa-file-archive-o" title="Measurements"></i></a>
                 <i class="fa fa-trash-o"></i>
