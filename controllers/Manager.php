cebe2e7be4dc089e33b67f65b312c5127132f392
Nikola <nikola.bojovic9@gmail.com>
2025-04-25 11:35:01 +0200

    123

--- controllers/Manager.php
+++ controllers/Manager.php
@@ -38,7 +38,7 @@ class Manager extends Controller
         $model = new Users($this->getDbAdapter());
 
         $data["users"] = $model->getAllTrainers($_GET["page"] ?? 1);
-        $count = $model->getTotalTr;
+        $count = $model->getTotalClientsCount();
         $data["pagination"] = $this->getPagination("/manager/index", $count, 10);
         $data["count"] = $count;
         
