788eddbddfb82e2f7dd22fc0b2f4bc78d12fcfc7
Nikola <nikola.bojovic9@gmail.com>
2025-05-16 12:12:24 +0200

    123

--- views/manager/success.php
+++ views/manager/success.php
@@ -1,63 +1,18 @@
-<h3 class="heading-style"><?=$this->translate("Checkout");?></h3>
-  <head>
-    <link rel="stylesheet">
-  </head>
-<div class="box box-primary">
-	<div class="box-body"></div>
-
-	<body>
-      <div class="card">
-      <div style="border-radius:200px; height:200px; width:200px; background: #f4f4f47d; margin:0 auto;">
-        <i class="checkmark">✓</i>
-      </div>
-        <h1>Success</h1> 
-        <p>Your subscription has been extended.<br/>Please restart the application!<br/>If you encounter some error, please contact support.</p>
-      </div>
-    </body>
-	<!-- <div class="box-footer clearfix no-border">
-    <button type="button" class="btn btn-default pull-right"><i class="fa fa-plus"></i> Add item</button>
-    </div> -->
-
-
-
-</div>
-
-<style>
-      body {
-        text-align: center;
-        padding: 40px 0;
-        background: #EBF0F5;
-      }
-        h1 {
-          color: #3d8cbc;
-          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
-          font-weight: 900;
-          font-size: 40px;
-          margin-bottom: 10px;
-        }
-        p {
-          color: #404F5E;
-          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
-          font-size:20px;
-          margin: 0;
-        }
-      i {
-        color: #3d8cbc;
-        font-size: 100px;
-        line-height: 200px;
-        margin-left:-15px;
-      }
-      .card {
-        background: white;
-        padding: 60px;
-        border-radius: 4px;
-        box-shadow: 0 2px 3px #C8D0D8;
-        display: inline-block;
-        margin: 0 auto;
-      }
-    </style>
-
-
-<script>
-
-</script>
+<?php
+  header("Refresh: 4; URL=myapp://paymentresult?status=success");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Redirecting...</title>
+</head>
+<body style="background: #EBF0F5; text-align:center; padding: 40px;">
+  <div class="card">
+    <div style="border-radius:200px; height:200px; width:200px; background: #f4f4f47d; margin:0 auto;">
+      <i class="checkmark">✓</i>
+    </div>
+    <h1>Success</h1> 
+    <p>Redirecting back to the app...</p>
+  </div>
+</body>
+</html>
